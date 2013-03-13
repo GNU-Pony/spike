69a2ead325160c7bf78e831caed811db70cde0ae
Mattias Andrée <maandree@operamail.com>
2013-03-13 10:26:38 +0100

    m
    Signed-off-by: Mattias Andrée <maandree@operamail.com>

--- spikedb.py
+++ spikedb.py
@@ -4,10 +4,11 @@
 import sys
 import os
 
+
 def fetch(db, values):
     return []
 
-def make(pairs):
+def make(db, pairs):
     pass
 
 
@@ -17,8 +18,8 @@ if len(sys.args) == 1:
         data.append(input())
     except:
         pass
-    make([(comb[:comb.find(' ')], comb[comb.find(' ') + 1:]) for comb in data])
+    make('testdb', [(comb[:comb.find(' ')], comb[comb.find(' ') + 1:]) for comb in data])
 else:
-    for pair in fetch('testdb', sort(sys.args[1:])):
+    for pair in fetch('testdb', sort([os.path.realpath(f) for f in sys.args[1:]])):
         print('%s --> %s' % pair)
 
