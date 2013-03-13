14c890c530c0ff9d3bcf6f88eb11f854fe31ceca
Mattias Andrée <maandree@operamail.com>
2013-03-13 12:50:09 +0100

    put fetch input into buckets internally sorted
    Signed-off-by: Mattias Andrée <maandree@operamail.com>

--- spikedb.py
+++ spikedb.py
@@ -10,10 +10,38 @@ INITIALS_LEN = 4
 # TODO paths should be db separated into groups by the binary logarithm of the length of their paths
 
 
-def fetch(db, maxlen, values):
-    return []
+# keep in mind that it we sould not depend on sort() using a stabil sort
 
-def make(db, maxlen, pairs): # keep in mind that it we sould not depend on sort() using a stabil sort
+
+def fetch(db, maxlen, values):
+    buckets = {}
+    for path in sort(values):
+        pos = 0
+        initials = ''
+        while '/' in path[pos : -1]:
+            pos = path.find('/') + 1
+            initials += path[pos]
+        while len(initials) < INITIALS_LEN:
+            pos += 1
+            if pos == len(initials):
+                break
+            initials += path[pos]
+        if len(initials) > INITIALS_LEN:
+            initials += initials[:INITIALS_LEN]
+        initials = [(ord(c) & 15) for c in initials]
+        ivalue = 0
+        for initial in initials:
+            ivalue = (ivalue << 4) | ivalue
+        if ivalue not in buckets:
+            buckets[ivalue] = []
+        buckets[ivalue].append(value)
+    rc = []
+    with open(db, 'rb') as file:
+        pass
+    return rc
+
+
+def make(db, maxlen, pairs):
     buckets = {}
     for pair in sort(pairs, key = lambda x : x[0]):
         pos = 0
@@ -71,6 +99,6 @@ if len(sys.args) == 1:
         pass
     make('testdb', 50, [(comb[comb.find(' ') + 1:], hash(comb[:comb.find(' ')]) & 0xFFFFFF) for comb in data])
 else:
-    for pair in fetch('testdb', 50, sort([os.path.realpath(f) for f in sys.args[1:]])):
+    for pair in fetch('testdb', 50, sort([os.path.realpath(f)[:50] for f in sys.args[1:]])):
         print('%s --> %s' % pair)
 
