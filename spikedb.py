7edd3cddd4240c0f3e025fbc0fe2837cdfe74626
Mattias Andrée <maandree@operamail.com>
2013-03-16 22:37:14 +0100

    merge the two draft files
    Signed-off-by: Mattias Andrée <maandree@operamail.com>

--- spikedb.py
+++ spikedb.py
@@ -13,6 +13,43 @@ INITIALS_LEN = 4
 # keep in mind that it we sould not depend on sorted() using a stabil sort
 
 
+def binsearch(list, item, min, max):
+    mid = 0
+    while min <= max:
+        mid = (min + max) >> 1
+        elem = list[mid]
+        if elem == item:
+            return mid
+        elif elem > item:
+            max = mid - 1
+        else:
+            mid += 1
+            min = mid
+    return ~mid
+
+
+def multibinsearch(list, items):
+    rc = []
+    count = len(items)
+    if count > 0:
+        minomax = [(0, count - 1, 0, len(list) - 1)]
+        while len(minomax) > 0:
+            (min, max, lmin, lmax) = minomax.pop()
+            while max != min:
+                (lastmax, lastlmax) = (max, lmax)
+                max = min + ((max - min) >> 1)
+                lmax = binsearch(list, items[max], lmin, lmax)
+                rc.append((max, lmax))
+                if lmax < 0:
+                    lmax = ~lmax
+                minomax.append((max + 1, lastmax, lmax + 1, lastlmax))
+        max = count - 1
+        lmax = binsearch(list, items[max], lmin, lmax)
+        rc.append((max, lmax))
+    return rc
+
+
+
 def fetch(db, maxlen, values):
     buckets = {}
     for path in sorted(values):
