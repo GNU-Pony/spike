7697803ad5e0d76b6ab58aede349aa44696b3662
Mattias Andrée <maandree@operamail.com>
2013-03-17 13:28:22 +0100

    document algorithms
    Signed-off-by: Mattias Andrée <maandree@operamail.com>

--- spikedb.py
+++ spikedb.py
@@ -14,6 +14,12 @@ INITIALS_LEN = 4
 
 
 def unique(sorted):
+    '''
+    Duplicate a sorted list and remove all duplicates
+    
+    @param   sorted:list<¿E?>  A sorted list, without leading `None`:s, more precisely, all equals elements must be grouped
+    @return  :list<¿E?>        `sorted` with all duplicates removed, for example: aaabbc → abc
+    '''
     rc = []
     last = None
     for element in sorted:
@@ -24,6 +30,15 @@ def unique(sorted):
 
 
 def binsearch(list, item, min, max):
+    '''
+    Find the index of an item in a list, with time complexity 𝓞(log n) and memory complexity 𝓞(1)
+    
+    @param   list:[int]→¿E?  Sorted list in which to search
+    @param   item:¿E?        Item for which to search
+    @param   min:int         The index of the first element in `list` for which to serach
+    @param   max:int         The index of the last element (inclusive) in `list` for which to serach
+    @return  :int            The index of `item` in `list`, if missing `~x` is returned were `x` is the position it would have if it existed
+    '''
     mid = 0
     while min <= max:
         mid = (min + max) >> 1
@@ -39,6 +54,13 @@ def binsearch(list, item, min, max):
 
 
 def multibinsearch(rc, list, items):
+    '''
+    Find the indices of multiple items in a list, with time complexity 𝓞(log n + m) and memory complexity 𝓞(log m) 
+    
+    @param  rc:append((int, int))→void  Object to which to append found items, the append items are of tuple (itemIndex:int, listIndex:int)
+    @param  list:[int]→¿E?;len()→int    Sorted list in which to search, the number of elements is named ‘n’ in the complexity analysis
+    @param  items:[int]→¿E?;len()→int   Sorted List of items for which to search, the number of elements is named ‘m’ in the complexity analysis
+    '''
     count = len(items)
     if count > 0:
         minomax = [(0, count - 1, 0, len(list) - 1)]
