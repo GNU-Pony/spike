ace76c30ffbac7b5fb8b69b69cb7332a2deeab3a
Mattias Andrée <maandree@operamail.com>
2013-03-27 01:49:52 +0100

    more commands
    Signed-off-by: Mattias Andrée <maandree@operamail.com>

--- dragonsuite.py
+++ dragonsuite.py
@@ -20,6 +20,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 import sys
 import os
+import re as regex
 import grp as groupmodule
 import pwd as usermodule
 from subprocess import Popen, PIPE
@@ -331,7 +332,7 @@ class DragonSuite():
             if mask == ~0:
                 os.lchmod(p, mode)
             else:
-                cur = os.stat(path).st_mode
+                cur = os.lstat(path).st_mode
                 os.lchmod(p, mode | (cur & ~mask))
     
     
@@ -344,14 +345,10 @@ class DragonSuite():
         @param  owner:int|str      The new owner, `-1` for ignored
         @param  group:int|str      The new group, `-1` for ignored, `-2` to select by owner
         '''
-        o = owner if isinstance(owner, int) else usermodule.getpwnam(owner).pw_uid
+        u = owner if isinstance(owner, int) else usermodule.getpwnam(owner).pw_uid
         g = group if isinstance(group, int) else groupmodule.getgrnam(group).gr_gid
         for p in ([path] if isinstance(path, str) else path):
-            if g == -2:
-                x = usermodule.getpwuid(os.stat(path).st_uid).pw_gid
-                os.lchown(p, o, x)
-            else:
-                os.lchown(p, o, g)
+            os.lchown(p, u, usermodule.getpwuid(os.lstat(path).st_uid).pw_gid if g == -2 else g)
     
     
     @staticmethod
@@ -576,13 +573,329 @@ class DragonSuite():
         
         @param  path:str|list<str>  New or updated entries
         '''
-        r = get('root', '/')
-        i = get('infodir', '/usr/share/info')
-        if not r.endswith('/'): r += '/'
-        if i.startswith('/'): i = i[1:]
-        if not i.endswith('/') and len(i) > 0: i += '/'
+        r = get('root', os.sep)
+        i = get('infodir', '%susr%sshare%sinfo' % (os.sep, os.sep, os.sep))
+        if not r.endswith(os.sep): r += os.sep
+        if i.startswith(os.sep): i = i[1:]
+        if not i.endswith(os.sep) and len(i) > 0: i += os.sep
         d = r + i + 'dir'
         for p in (['--', path] if isinstance(path, str) else (['--'] + path)):
             execute(['install-info', '--', p, d], fail = False)
+    
+    
+    @staticmethod
+    def mv(source, destination):
+        '''
+        Move one or more files
+        
+        @param  source:str|itr<str>  The files to move
+        @param  destination:str      The destination, either directory or new file
+        '''
+        ps = [source] if isinstance(source, str) else source
+        d = destination if destination.endswith(os.sep) else (destination + os.sep)
+        if len(ps) == 1:
+            if os.path.exists(destination) and os.path.isdir(destination):
+                os.rename(source, d + basename(source))
+            else:
+                os.rename(source, destination)
+        else:
+            if not os.path.exists(destination):
+                raise OSError('Destination %s does not exist but must be a directory' % destination)
+            elif not os.path.isdir(destination):
+                raise OSError('Destination %s exists and is not a directory' % destination)
+            else:
+                for p in ps:
+                    os.rename(p, d + basename(p))
+    
+    
+    @staticmethod
+    def echo(text, newline = True, stderr = False):
+        '''
+        Display a line of text
+        
+        @param  text:str|list<str>  The text
+        @param  newline:bool        Whether to end with a line break
+        @param  stderr:bool         Whether to print to stderr
+        '''
+        s = sys.stdout.buffer if not stderr else sys.stderr.buffer
+        if isinstance(text, str):
+            s.write((text + ('\n' if newline else '')).encode('utf-8'))
+        else
+            s.write(('\n'.join(text) + ('\n' if newline else '')).encode('utf-8'))
+    
+    
+    @staticmethod
+    def cp(source, destination, recursive = True):
+        '''
+        Copies files and directories, note that you can do so much more with GNU ocreutils's cp
+        
+        @param  source:str|itr<str>   Files to copy
+        @param  destination:str       Destination filename or directory
+        '''
+        install(source, destination, parents=False, recursive = recursive, savemode=True, preservecontext=False)
+    
+    
+    @staticmethod
+    def cp_r(source, destination):
+        '''
+        Copies files and directories, recursively
+        
+        @param  source:str|itr<str>   Files to copy
+        @param  destination:str       Destination filename or directory
+        '''
+        cp(source, destination, True)
+    
+    
+    @staticmethod
+    def install(source, destination, owner = -1, group = -1, mode = -1, strip = False,
+                directory = False, parents = True, recursive = True, savemode = True):
+        '''
+        Copies files and set attributes
+        
+        @param  source:str|itr<str>   Files to copy
+        @param  destination:str       Destination filename or directory
+        @param  owner:int|str         The new owner, `-1` for preserved
+        @param  group:int|str         The new group, `-1` for preserved, `-2` to select by owner
+        @param  mode:int              The desired protection bits, `-1` for preserved
+        @param  strip:bool            Whether to strip symbol table
+        @param  directory:bool        Whether treat all source files is directory names
+        @param  parents:bool          Whether create missing directories
+        @param  recursive:bool        Copy directories resursively
+        @param  savemode:bool         Whether to use the protection bits of already installed versions
+        '''
+        ps = [source] if isinstance(source, str) else source
+        d = destination if destination.endswith(os.sep) else (destination + os.sep)
+        pairs = None
+        if len(ps) == 1:
+            if os.path.exists(destination) and os.path.isdir(destination):
+                pairs = [(source, d + basename(source))]
+            else:
+                pairs = [source, destination)]
+        else:
+            if not os.path.exists(destination):
+                if parents:
+                    mkdir_p(destination)
+                else:
+                    raise OSError('Destination %s does not exist but must be a directory' % destination)
+            elif not os.path.isdir(destination):
+                raise OSError('Destination %s exists and is not a directory' % destination)
+            else:
+                for p in ps:
+                    pairs = [p, d + basename(p)]
+        for (src, dest) in pairs:
+            protection = mode
+            if savemode and os.path.exists(dest):
+                protection = os.lstat(dest).st_mode
+            elif mode < 0:
+                protection = 0x755 if directory else os.lstat(src).st_mode
+            if directory or os.path.isdir(src):
+                mkdir(dest)
+            else:
+                with open(src, 'rb') as ifile:
+                    with open(dest, 'wb') as ofile:
+                        ofile.write(ifile.read()) # TODO: support large files by copying in chunks
+            (u, g) = (owner, group)
+            stat = os.lstat(src)
+            u = u if isinstance(u, str) or u != -1 else stat.st_uid
+            g = g if isinstance(g, str) or g != -1 else stat.st_gid
+            chown(dest, u, g)
+            os.lchmod(dest, protection)
+            if strip and not directory:
+                strip(dest)
+            if recursive and os.path.isdir(src) and not directory:
+                d = dest if dest.endswith(os.sep) else (dest + os.sep)
+                sources = [d + f for f in os.listdir()]
+                install(sources, dest, owner, group, mode, strip, False, False, True, savemode)
+    
+    
+    @staticmethod
+    def find(path, maxdepth = -1, hardlinks = True):
+        '''
+        Gets all existing subfiles, does not follow links including hardlinks
+        
+        @param   path:str|itr<str>  Search root or roots
+        @param   maxdepth:int       Maximum search depth, `-1` for unbounded
+        @param   hardlinks:bool     Whether to list all files with same inode number rather than just the first
+        @return  :list<str>         Found files
+        '''
+        rc = []
+        visited = set()
+        stack = [path] if isinstance(path, str) else path
+        stack = [(e, 0) for e in stack]
+        while len(stack) > 0:
+            (f, d) = stack.pop()
+            if os.path.exists(f):
+                f = f if not f.endswith(os.sep) else f[:-1]
+                if (not os.path.islink(f)) and os.path.isdir(f):
+                    inode = os.lstat(f).st_ino
+                    if inode not in visited:
+                        visited.add(inode)
+                        f += os.sep
+                        if d != maxdepth:
+                            d += 1
+                            for sf in os.listdir():
+                                stack.append((f + sf, d))
+                    elif not hardlinks:
+                        continue
+                elif not hardlinks:
+                    inode = os.lstat(f).st_ino
+                    if inode in visited:
+                        continue
+                    visited.add(inode)
+                rc.append(f)
+        return rc
+    
+    
+    @staticmethod
+    def path(exprs, existing = False):
+        '''
+        Gets files matching a pattern
+        
+        Here     => Regular expression
+        {a,b,c}  => (a|b|c)
+        {a..g}   => [ag]
+        {1..11}  => (1|2|3|4|5|6|7|8|9|10|11)
+        *        => .*
+        ?        => .
+        
+        Everything else if matched verbosely and the matching is closed (regex: ^pattern$),
+        and \ is used to escape characters do that they are matched verbosely instead of
+        getting a special meaning.
+        Ending the pattern with / specified that it should be a directory.
+        
+        @param   exprs:str|itr<str>  Expressions
+        @param   existing:bool       Whether to only match to existing files
+        @return  :list<str>          Matching files
+        '''
+        def _(expr)
+            ps = ['']
+            esc = False
+            buf = ''
+            b = 0
+            for c in expr.replace('\0', '').replace('/', ('\\' if os.sep in '?*{},.\\' else '') + os.sep):
+                if esc:
+                    esc = False
+                    if b > 0:
+                        buf += '\\'
+                    buf += c
+                elif c == '\\': esc = True
+                elif c == '?':  buf += '\0?' if b == 0 else '?'
+                elif c == '*':  buf += '\0*' if b == 0 else '*'
+                elif c == ',':  buf += '\0,' if b == 1 else ','
+                elif c == '.':  buf += '\0.' if b == 1 else '.'
+                elif c == '{':
+                    if b == 0:
+                        ps = [p + buf for p in ps]
+                        buf = ''
+                    b += 1
+                elif c == '}':
+                    if b == 1:
+                        t = [_(tp) for tp in buf.split('\0,')]
+                        pz = []
+                        for a in ps:
+                            for b in t:
+                                if '\0.' not in b:
+                                    pz.append(a + b)
+                                elif (len(b) - len(b.replace('\0.', '')) != 4) and ('\0.\0.' not in b):
+                                    pz.append(a + b.replace('\0.', '.'))
+                                elif b.startswith('\0.') or b.endswith('\0.')
+                                    pz.append(a + b.replace('\0.', '.'))
+                                else:
+                                    (l, r) = b.split('\0.\0.')
+                                    if len(string.strip(l + r, '0123456789')) == 0:
+                                        step = 1 if int(l) <= int(r) else -1
+                                        x = int(l) if step == 1 else int(r)
+                                        n = int(r) if step == 1 else int(l)
+                                        while x != n:
+                                            pz.append(a + str(x))
+                                            x += step
+                                        pz.append(a + str(x))
+                                    elif len(l) == 1 and len(r) == 1:
+                                        step = 1 if ord(l) <= ord(r) else -1
+                                        x = ord(l) if step == 1 else ord(r)
+                                        n = ord(r) if step == 1 else ord(l)
+                                        while x != n:
+                                            pz.append(a + chr(x))
+                                            x += step
+                                        pz.append(a + chr(x))
+                                    else:
+                                        pz.append(a + b.replace('\0.', '.'))
+                        ps = pz
+                        pz = None
+                        buf = ''
+                    else:
+                        buf += '}'
+                    b -= 1
+                else:
+                    buf += c
+            return [p + buf for p in ps]
+        rc = []
+        for expr in ([exprs] if isinstance(exprs, str) else exprs):
+            for p in _(expr):
+                if '\0' not in p:
+                    rc.append(p)
+                else:
+                    f = ['']
+                    if os.sep == '?': p = p.replace('\0?', '\0a')
+                    if os.sep == '*': p = p.replace('\0*', '\0b')
+                    parts = p.split(os.sep)
+                    if os.sep == '?': parts = [p.replace('\0a', '\0?') for p in parts]
+                    if os.sep == '*': parts = [p.replace('\0b', '\0*') for p in parts]
+                    for p in parts:
+                        if len(p) == 0:
+                            pass
+                        elif p == '.':
+                            f = [(os.curdir if len(_) == 0 else _) for _ in f]
+                        elif p == '..':
+                            _f = []
+                            for _ in f:
+                                if len(_) == 0:
+                                    _ = os.pardir
+                                elif _ == os.pardir or _.endswith(os.sep + os.pardir):
+                                    _ += os.sep + os.pardir
+                                else:
+                                    _ = _[:_.rfind('/')]
+                                _f.append(_)
+                            f = _f
+                        elif '\0' not in p:
+                            f = [(p if len(_) == 0 else (_ + os.sep + p)) for _ in f]
+                        else:
+                            _f = []
+                            for _ in f:
+                                root = os.curdir if len(_) == 0 else _
+                                if os.path.exists(root) and not os.path.isdir(root):
+                                    break
+                                subs = os.listdir(root)
+                                matches = []
+                                s = p
+                                for c in '\\.^+$[]|(){}':
+                                    s = s.replace(c, '\\' + c)
+                                _ = ''
+                                esc = False
+                                for c in s:
+                                    if esc:
+                                        _ += {'\0*' : '.*',  '\0?' : '.'}[c]
+                                        esc = False
+                                    elif c == '\0':
+                                        esc = True
+                                    else:
+                                        _ += c
+                                s = '^' + _.replace('?', '\\?').replace('*', '\\*') + '$'
+                                matcher = regex.compile(s, regex.DOTALL)
+                                for s in subs:
+                                    if matcher.match(s) is not None:
+                                        matches.append(s)
+                                if len(_) > 0:
+                                    _ += os.sep
+                                for m in matches:
+                                    _f.append(_ + m)
+                            f = _f
+        if not existing:
+            return rc
+        nrc = []
+        for p in rc:
+            if os.path.exists(p) and ((not p.endswith(os.sep)) or os.path.isdir(p)):
+                nrc.append(p)
+        return nrc
 
 
