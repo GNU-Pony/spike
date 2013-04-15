34059e28c85a04b2cf46dd41b7522cd362284afa
Mattias Andrée <maandree@operamail.com>
2013-04-15 21:42:52 +0200

    split spike database into spike database and algorithmic spike
    Signed-off-by: Mattias Andrée <maandree@operamail.com>

--- spikedb.py
+++ spikedb.py
@@ -3,51 +3,12 @@
 
 import sys
 import os
+from algospike from *
 
 INITIALS_LEN = 4
 
 # TODO paths should be db separated into groups by the binary logarithm of the length of their paths
 # TODO make really sure that file.read(n) reads exactly n bytes (via fileread(stream, n) function)
-
-
-def unique(sorted):
-    '''
-    Duplicate a sorted list and remove all duplicates
-    
-    @param   sorted:list<¿E?>  A sorted list, without leading `None`:s, more precisely, all equals elements must be grouped
-    @return  :list<¿E?>        `sorted` with all duplicates removed, for example: aaabbc → abc
-    '''
-    rc = []
-    last = None
-    for element in sorted:
-        if element != last:
-            last = element
-            rc.append(element)
-    return rc
-
-
-def binsearch(list, item, min, max):
-    '''
-    Find the index of an item in a list, with time complexity 𝓞(log n) and memory complexity 𝓞(1)
-    
-    @param   list:[int]→¿E?  Sorted list in which to search
-    @param   item:¿E?        Item for which to search
-    @param   min:int         The index of the first element in `list` for which to serach
-    @param   max:int         The index of the last element (inclusive) in `list` for which to serach
-    @return  :int            The index of `item` in `list`, if missing `~x` is returned were `x` is the position it would have if it existed
-    '''
-    mid = 0
-    while min <= max:
-        mid = (min + max) >> 1
-        elem = list[mid]
-        if elem == item:
-            return mid
-        elif elem > item:
-            max = mid - 1
-        else:
-            mid += 1
-            min = mid
-    return ~mid
 
 
 def __blocksize(file):
@@ -61,32 +22,6 @@ def __blocksize(file):
         return os.stat(os.path.realpath(file)).st_blksize
     except:
         return 8 << 10
-
-
-def multibinsearch(rc, list, items):
-    '''
-    Find the indices of multiple items in a list, with time complexity 𝓞(log n + m) and memory complexity 𝓞(log m) 
-    
-    @param  rc:append((int, int))→void  Object to which to append found items, the append items are of tuple (itemIndex:int, listIndex:int)
-    @param  list:[int]→¿E?;len()→int    Sorted list in which to search, the number of elements is named ‘n’ in the complexity analysis
-    @param  items:[int]→¿E?;len()→int   Sorted list of items for which to search, the number of elements is named ‘m’ in the complexity analysis
-    '''
-    count = len(items)
-    if count > 0:
-        minomax = [(0, count - 1, 0, len(list) - 1)]
-        while len(minomax) > 0:
-            (min, max, lmin, lmax) = minomax.pop()
-            while max != min:
-                (lastmax, lastlmax) = (max, lmax)
-                max = min + ((max - min) >> 1)
-                lmax = binsearch(list, items[max], lmin, lmax)
-                rc.append((max, lmax))
-                if lmax < 0:
-                    lmax = ~lmax
-                minomax.append((max + 1, lastmax, lmax + 1, lastlmax))
-        max = count - 1
-        lmax = binsearch(list, items[max], lmin, lmax)
-        rc.append((max, lmax))
 
 
 class Blocklist():
