541486f077ac4f756fd0c63a0b102781a6252675
Mattias Andrée <maandree@operamail.com>
2013-04-17 21:05:59 +0200

    m
    Signed-off-by: Mattias Andrée <maandree@operamail.com>

--- src/sha3sum.py
+++ src/sha3sum.py
@@ -21,25 +21,16 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import sys
 import os
+# TODO continue adapting
 
 
 class SHA3:
     '''
-    SHA-3/Keccak[] hash algorithm implementation
+    SHA-3/Keccak[(r = 1024, c = 576, n = 576)] hash algorithm implementation
     
     @author  Mattias Andrée  (maandree@member.fsf.org)
     '''
     
-    
-    RC=[0x0000000000000001, 0x0000000000008082, 0x800000000000808A, 0x8000000080008000,
-        0x000000000000808B, 0x0000000080000001, 0x8000000080008081, 0x8000000000008009,
-        0x000000000000008A, 0x0000000000000088, 0x0000000080008009, 0x000000008000000A,
-        0x000000008000808B, 0x800000000000008B, 0x8000000000008089, 0x8000000000008003,
-        0x8000000000008002, 0x8000000000000080, 0x000000000000800A, 0x800000008000000A,
-        0x8000000080008081, 0x8000000000008080, 0x0000000080000001, 0x8000000080008008]
-    '''
-    :list<int>  Round contants
-    '''
     
     B = [0] * 25
     '''
