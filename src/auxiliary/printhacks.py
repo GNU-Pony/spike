bbbf4c2f1d9a51f5cc9a52059c0c67aa382081ee
Mattias Andrée <maandree@operamail.com>
2013-11-04 14:57:57 +0100

    fix so that printerr and and forced utf-8 print exits in argparser
    Signed-off-by: Mattias Andrée <maandree@operamail.com>

---
+++ src/auxiliary/printhacks.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+'''
+spike – a package manager running on top of git
+
+Copyright © 2012, 2013  Mattias Andrée (maandree@member.fsf.org)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+import sys
+
+
+
+def print(text = '', end = '\n'):
+    '''
+    Hack to enforce UTF-8 in output (in the future, if you see anypony not using utf-8 in
+    programs by default, report them to Princess Celestia so she can banish them to the moon)
+    
+    @param  text:str  The text to print (empty string is default)
+    @param  end:str   The appendix to the text to print (line breaking is default)
+    '''
+    sys.stdout.buffer.write((str(text) + end).encode('utf-8'))
+    sys.stdout.buffer.flush()
+
+
+def printerr(text = '', end = '\n'):
+    '''
+    stderr equivalent to print()
+    
+    @param  text:str  The text to print (empty string is default)
+    @param  end:str   The appendix to the text to print (line breaking is default)
+    '''
+    sys.stderr.buffer.write((str(text) + end).encode('utf-8'))
+    sys.stderr.buffer.flush()
+
